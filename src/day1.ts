6b792e67398ea883213667671d38375215266862
Prowler68 <mikeri@seznam.cz>
2022-12-05 16:14:21 +0100

    Day 2

--- src/day1.ts
+++ src/day1.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 
 export function day1() {
+  console.log("## Day 1 ##");
 
   const inventory = readFileSync("./inputs/inputDay1.txt", {encoding: "utf-8"}).split("\n\n");
   const sortedInventory = inventory.map(a => a.split("\n").map(Number).reduce((a,b) => a + b, 0));
