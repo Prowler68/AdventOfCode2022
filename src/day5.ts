5129c2c8d51b3f2b76f16bea6e0af5f2ad6a9462
Prowler68 <mikeri@seznam.cz>
2022-12-09 00:50:07 +0100

    Day 5

--- src/day5.ts
+++ src/day5.ts
@@ -20,14 +20,57 @@ export function day5() {
 
       for (let i = 0; i < row.length; i++) {
         const char = row.charAt(i);
-        if (char.includes("-")) {
-        } else {
+        if (!char.includes("-")) {
           columns[i].push(char);
         }
       }
     }
     return columns;
   };
-  console.log(crates);
-  console.log("Result1:", prepareStock(crates));
+
+  const crane = (instructions: string[], stock: string[][], model: "CrateMover9000" | "CrateMover9001") => {
+    let result = stock;
+    for (let i = 0; i < instructions.length; i++) {
+      const command = instructions[i]
+        .replaceAll("move ", "")
+        .replaceAll(" from ", "-")
+        .replaceAll(" to ", "-")
+        .split("-");
+      const count = Number(command[0]);
+      const target = Number(command[1]);
+      const destination = Number(command[2]);
+
+      if (model === "CrateMover9000") {
+        for (let index = 0; index < count; index++) {
+          const liftedCrate = result[target - 1].pop();
+          if (liftedCrate) {
+            result[destination - 1].push(liftedCrate);
+          }
+        }
+      }
+
+      if (model === "CrateMover9001" && count && target && destination) {
+        const liftedStack = result[target - 1].splice(-count, count);
+        result[destination - 1].push(...liftedStack);
+      }
+    }
+    return result;
+  };
+
+  const getTopCrates = (stock: string[][]) => {
+    let result = "";
+    for (let i = 0; i < stock.length; i++) {
+      const topCrate = stock[i].pop();
+      if (topCrate) {
+        result = result + topCrate;
+      }
+    }
+    return result;
+  };
+
+  const result1 = getTopCrates(crane(instructions, prepareStock(crates), "CrateMover9000"));
+  const result2 = getTopCrates(crane(instructions, prepareStock(crates), "CrateMover9001"));
+
+  console.log("Result1", result1);
+  console.log("Result2", result2);
 }
