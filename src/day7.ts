c737de28d5d3bef73cc0bbd39742553a478f2006
Richard Mikeska <mikeri@seznam.cz>
2022-12-11 22:11:07 +0100

    Day 7

--- src/day7.ts
+++ src/day7.ts
@@ -1,39 +1,15 @@
-import { Dir, readFileSync } from "fs";
+import { readFileSync } from "fs";
 
 export function day7() {
   console.log("## Day 7 ##");
 
-  // type File = {
-  //   parent: string;
-  //   name: string;
-  //   size: number;
-  // };
-
-  // type Dir = {
-  //   parent: string;
-  //   name: string;
-  //   content: {
-  //     files: string[];
-  //     directories: string[];
-  //   };
-  // };
-
   type Dir = { type: "dir"; name: string; parent?: Item; content: Item[] };
   type File = { type: "file"; name: string; parent: Item; size: number };
-  type Item =
-    | { type: "dir"; name: string; parent?: Item; content: Item[] }
-    | { type: "file"; name: string; parent: Item; size: number };
+  type Item = Dir | File;
 
-  // const directories: Dir[] = [{ name: "/", parent: "", content: { files: [], directories: [] } }];
-  // const files: File[] = [];
-  const storage: Item = { name: "/", type: "dir", content: [] };
+  const input = readFileSync("./inputs/inputDay7.txt", { encoding: "utf-8" }).split("\n");
 
-  const input = readFileSync("./inputs/inputDay7.txt", { encoding: "utf-8" }).replaceAll("\r", "").split("\n");
-  const testInput = readFileSync("./inputs/testInputDay7.txt", { encoding: "utf-8" }).replaceAll("\r", "").split("\n");
-
-  // const isDir = (item: Item) => {
-  //   return item.type === "dir";
-  // };
+  const storage: Dir = { name: "/", type: "dir", content: [] };
 
   const readConsole = (input: string[]) => {
     let target: Item = storage;
@@ -50,68 +26,80 @@ export function day7() {
           } else if (consoleLine.substring(5, 7).includes("/")) {
             target = storage;
           } else {
-            const newTarget = storage.content.find(a => a.name === consoleLine.substring(5, consoleLine.length));
-            if (newTarget) {
-              target = newTarget;
+            if (target.type === "dir") {
+              target = target.content.find(a => a.name === consoleLine.substring(5, consoleLine.length)) ?? target;
             }
           }
-        }
-        if (consoleLine.substring(2, 5).includes("ls")) {
+        } else if (consoleLine.substring(2, 5).includes("ls")) {
           list = true;
         }
       } else if (list) {
         if (consoleLine.substring(0, 3).includes("dir")) {
           if (target.type === "dir") {
-            storage.content
-              .find((a): a is Dir => a.name === target.name && a.parent === target.parent)
-              ?.content.push({
-                type: "dir",
-                name: consoleLine.substring(4),
-                parent: target,
-                content: [],
-              });
+            target.content.push({
+              type: "dir",
+              name: consoleLine.substring(4),
+              parent: target,
+              content: [],
+            });
+          } else {
           }
         } else {
           const file = consoleLine.split(" ");
-          storage.content
-            .find((a): a is Dir => a.name === target.name && a.parent === target.parent)
-            ?.content.push({ type: "file", name: file[1], parent: target, size: Number(file[0]) });
+          if (target.type === "dir") {
+            target.content.push({ type: "file", name: file[1], parent: target, size: Number(file[0]) });
+          }
         }
       }
     }
   };
 
-  // const getDirSize = (directory: Dir) => {
-  //   let sum = 0;
-  //   for (let i = 0; i < directory.content.files.length; i++) {
-  //     const fileName = directory.content.files[i];
-  //     const value = files.find(a => a.name === fileName && a.parent === directory.name)?.size;
-  //     if (value) {
-  //       sum += value;
-  //     }
-  //   }
+  const flattenStorage = (directory: Dir) => {
+    const flatStorage: Dir[] = [];
+    flatStorage.push(directory);
+    for (let i = 0; i < directory.content.length; i++) {
+      const item = directory.content[i];
+      if (item.type === "dir") {
+        flatStorage.push(...flattenStorage(item));
+      }
+    }
+    return flatStorage;
+  };
 
-  //   for (let i = 0; i < directory.content.directories.length; i++) {
-  //     const dirName = directory.content.directories[i];
-  //     const child = directories.find(a => a.name === dirName && a.parent === directory.name);
-  //     if (child) {
-  //       sum += getDirSize(child);
-  //     }
-  //   }
-  //   return sum;
-  // };
+  const getDirSize = (directory: Dir) => {
+    let sum = 0;
+    for (let i = 0; i < directory.content.length; i++) {
+      const item = directory.content[i];
+      if (item.type === "file") {
+        sum += item.size;
+      } else if (item.type === "dir") {
+        sum += getDirSize(item);
+      }
+    }
+    return sum;
+  };
 
-  // const findDirBySize = (limit: number) => {
-  //   const sums = [];
-  //   for (let i = 0; i < directories.length; i++) {
-  //     const directory = directories[i];
-  //     sums.push(getDirSize(directory));
-  //   }
-  //   return sums.filter(a => a <= limit).reduce((a, b) => a + b, 0);
-  // };
+  const findDirBySize = (limit: number, discSpace?: number) => {
+    const sums = [];
+    for (let i = 0; i < flatStorage.length; i++) {
+      const directory = flatStorage[i];
+      sums.push(getDirSize(directory));
+    }
+    if (!discSpace) {
+      return sums.filter(a => a <= limit).reduce((a, b) => a + b, 0);
+    } else {
+      return Math.min(...sums.filter(a => a > limit - (discSpace - getDirSize(flatStorage[0]))));
+    }
+  };
 
-  readConsole(testInput);
+  const findClosest = (discSpace: number) => {
+    const targetSize = discSpace - getDirSize(flatStorage[0]);
+    return findDirBySize(targetSize);
+  };
 
-  console.log("storage: ", storage.content);
-  // console.log("Result1:", findDirBySize(100000));
+  readConsole(input);
+  const flatStorage = flattenStorage(storage);
+
+  console.log("Result1: ", findDirBySize(100000));
+  console.log("Result2: ", findDirBySize(30000000, 70000000));
 }
---
+++ src/day8.ts
@@ -0,0 +1,6 @@
+import { readFileSync } from "fs";
+
+export function day8() {
+  console.log("## Day 8 ##");
+  const input = readFileSync("./inputs/inputDay7.txt", { encoding: "utf-8" });
+}
